65ba2cad8387a98d41a3986b92932af4b42c745f
Leo Yang <leo19941227@gmail.com>
2017-11-15 11:17:17 +0800

    my try

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
 var answer = Math.floor(Math.random() * 100) + 1;
+
+var input = document.querySelector(".guessField");
+var submit = document.querySelector(".guessSubmit");
+
+var history = document.querySelector(".guesses");
+var lastResult = document.querySelector(".lastResult");
+var lowOrHigh = document.querySelector(".lowOrHi");
+
+var guessCount = 1;
+
+function evaluateGuess() {
+  var inputNumber = Number(input.value);
+
+  if(guessCount === 1) {
+    history.textContent = "Previous guesses:";
+  }
+  //history.textContent += (' ' + inputNumber);
+  
+  if(inputNumber === answer) {
+    lowOrHigh.textContent = "Correct!";
+  } else if(inputNumber < answer) {
+    lowOrHigh.textContent = "Too low";
+  } else {
+    lowOrHigh.textContent = "Too high";
+  }
+  
+  guessCount += 1;
+  input.value = '';
+  input.focus();
+}
+
+submit.onclick = function() {
+  evaluateGuess();
+}
