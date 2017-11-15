cdf17ad8705322a3fa1a67a9fbbf03a47a2c79ce
Leo Yang <leo19941227@gmail.com>
2017-11-15 23:49:26 +0800

    11/15

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,21 @@ function Cell(x, y) {
 }
 
 var size = 10;
+var plane = new Array(size);
+var tmp = document.createElement('div');
+tmp.id = 'cells';
+tmp.width = '50%';
+document.body.appendChild(tmp);
+var cells = document.querySelector('#cells');
+
+for(var i=0; i<size; i++) {  
+  plane[i] = new Array(size);
+  for(var j=0; j<size; j++) {
+    var tmp = document.createElement('button');
+    tmp.style.margin = '0 auto';
+    tmp.style.padding = '40px';
+    tmp.style.backgroundColor = 'yellow';
+    cells.appendChild(tmp);
+  }
+}
+
