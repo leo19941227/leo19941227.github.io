d6fcc65266cbf9d40e19244f1c41541c5868fc6d
Leo Yang <leo19941227@gmail.com>
2017-11-16 03:36:48 +0800

    color adjust

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
-var size = 12;
-var pixel = 50;
-var bumbNum = 15;
+//var size = Number( prompt('Enter the metrix size you want to play: ') );
+var size = 10;
+var pixel = 55;
+var bumbNum = Math.floor(4 * size / 5);
 var saveNum = size * size - bumbNum;
 var plane = new Array(size);
+var bombList = new Array(size * size);
 
 var m_offset = [-1, -1, -1, 0, 0, 0, 1, 1, 1];
 var n_offset = [-1, 0, 1, -1, 0, 1, -1, 0, 1];
+
+var h2 = document.querySelector('h2');
+h2.textContent = 'There are ' + saveNum + ' steps to go';
 
 function Cell(m, n) {
   var row = m;
@@ -13,48 +18,67 @@ function Cell(m, n) {
   var open = false;
   var bomb = false;
   var button = document.querySelector('#' + 'R' + row + 'C' + column);
-  
-  this.setBomb = function() {
+
+  this.setBomb = function () {
     bomb = true;
     button.textContent = 'X';
   }
-  this.isBomb = function() {
+  this.isBomb = function () {
     return bomb;
   }
-  this.stopFunction = function() {
+  this.bombing = function () {
+    button.style.color = 'white';
+    button.style.backgroundColor = 'red'
+    button.textContent = 'X';
+  }
+  this.stopFunction = function () {
     button.disabled = true;
-  }  
-  this.clicked = function() {
-    if(!open) {
+  }
+
+  this.clicked = function () {
+    if (!open) {
       open = true;
-      if(bomb) {
-        button.style.color = 'white';
-        button.style.backgroundColor = 'red'
-        button.textContent = 'X';
-        var endMessage = document.querySelector('h2');
-        endMessage.textContent = 'YOU LOSE';
-        endMessage.style.color = 'darkorange';
+      saveNum -= 1;
+      h2.textContent = 'There are ' + saveNum + ' steps to go';
+      if (bomb) {
+        for (var i = 0; i < bumbNum; i++) {
+          var setBomb = bombList[i];
+          plane[Math.floor(setBomb / size)][setBomb % size].bombing();
+        }
+        h2.textContent = 'YOU LOSE';
+        h2.style.color = 'darkorange';
         gameOverHandler();
       } else {
+        if (saveNum === 0) {
+          for (var i = 0; i < bumbNum; i++) {
+            var setBomb = bombList[i];
+            plane[Math.floor(setBomb / size)][setBomb % size].bombing();
+          }
+          var endMessage = document.querySelector('h2');
+          h2.textContent = 'YOU WIN';
+          h2.style.color = 'darkseagreen';
+          gameOverHandler();
+        }
+
         var bombSum = 0;
-        for(var i=0; i<9; i++) {
+        for (var i = 0; i < 9; i++) {
           var r = row + m_offset[i];
           var c = column + n_offset[i];
-          if(r >= 0 && r < size && c >= 0 && c < size) {
-            if(plane[r][c].isBomb()) {
+          if (r >= 0 && r < size && c >= 0 && c < size) {
+            if (plane[r][c].isBomb()) {
               bombSum += 1;
             }
           }
         }
         button.style.color = 'white';
-        button.style.backgroundColor = 'green';
+        button.style.backgroundColor = '#8fdb84';
         button.textContent = bombSum;
-        
-        if(bombSum === 0) {
-          for(var i=0; i<9; i++) {
+
+        if (bombSum === 0) {
+          for (var i = 0; i < 9; i++) {
             var r = row + m_offset[i];
             var c = column + n_offset[i];
-            if(r >= 0 && r < size && c >= 0 && c < size) {
+            if (r >= 0 && r < size && c >= 0 && c < size) {
               plane[r][c].clicked();
             }
           }
@@ -62,8 +86,8 @@ function Cell(m, n) {
       }
     }
   }
-  
-  button.addEventListener('click', this.clicked); 
+
+  button.addEventListener('click', this.clicked);
 }
 
 var tmp = document.createElement('div');
@@ -73,46 +97,45 @@ tmp.style.margin = '0 auto';
 document.body.appendChild(tmp);
 var cells = document.querySelector('#cells');
 
-for(var i = 0; i < size; i++) {  
+for (var i = 0; i < size; i++) {
   plane[i] = new Array(size);
-  for(var j = 0; j < size; j++) {
+  for (var j = 0; j < size; j++) {
     var tmp = document.createElement('button');
     tmp.style.margin = '0 auto';
     tmp.style.width = pixel + 'px';
     tmp.style.height = pixel + 'px';
     tmp.textContent = 'NaN';
-    tmp.style.color = 'yellow';
-    tmp.style.backgroundColor = 'yellow';
-    
+    tmp.style.color = '#FFFFBB';
+    tmp.style.backgroundColor = '#FFFFBB';
+    tmp.style.fontSize = '18px';
+
     tmp.id = 'R' + i + 'C' + j;
     cells.appendChild(tmp);
-    
+
     plane[i][j] = new Cell(i, j);
   }
 }
 
 // plane settings is done, can use 'plane' and 'clicked' to finish the rest.
 
-var numList = new Array(size * size);
-for(var i = 0; i < size*size; i++) {
-  numList[i] = i;
+for (var i = 0; i < size * size; i++) {
+  bombList[i] = i;
 }
-for(var i = 1; i < size*size; i++) {
+for (var i = 1; i < size * size; i++) {
   var target = Math.floor(Math.random() * i);
-  var tmp = numList[i];
-  numList[i] = numList[target];
-  numList[target] = tmp;
+  var tmp = bombList[i];
+  bombList[i] = bombList[target];
+  bombList[target] = tmp;
 }
 
-for(var i = 0; i < bumbNum; i++) {
-  setBomb = numList.pop();
-  plane[ Math.floor(setBomb/size) ][ setBomb % size ].setBomb();
+for (var i = 0; i < bumbNum; i++) {
+  var setBomb = bombList[i];
+  plane[Math.floor(setBomb / size)][setBomb % size].setBomb();
 }
 
 function gameOverHandler() {
-  alert('over');
-  for(var i = 0; i < size; i++) {
-    for(var j = 0; j < size; j++) {
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
       plane[i][j].stopFunction();
     }
   }
